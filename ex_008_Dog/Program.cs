2dd2bc70ec1738db5581b1c37bcc88608dfe7cff
VadimR <rakhmaninvs@gmail.com>
2022-09-15 02:33:56 +0300

    ex_008

---
+++ ex_008_Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0,
+    distance = 10000,
+    firstFriendSpeed = 1,
+    secondFriendSpeed = 2,
+    dogSpeed = 5,
+    friend = 2,
+    time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine("Dog will run " + count + " times");
