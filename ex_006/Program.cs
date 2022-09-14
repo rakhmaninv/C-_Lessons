6255d0322eb2b44db46ca9f665054d41a8546947
VadimR <rakhmaninvs@gmail.com>
2022-09-14 22:21:01 +0300

    ex_006

---
+++ ex_006/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 2;
+int b = 5;
+int c = 1;
+int d = 9;
+int e = 7;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine("max = " + max);
