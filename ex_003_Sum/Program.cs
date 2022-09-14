5a68f1c456347ac6f57173341b2bb61f0052aa56
VadimR <rakhmaninvs@gmail.com>
2022-09-14 18:01:43 +0300

    changed ex_003 random

--- ex_003_Sum/Program.cs
+++ ex_003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numbA = 3;
-int numbB = 7;
+﻿int numbA = new Random().Next(1,10);
+Console.WriteLine(numbA);
+int numbB = new Random().Next(1,10);
+Console.WriteLine(numbB);
 int result = numbA + numbB;
 Console.WriteLine(result);
