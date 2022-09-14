ee011b66e35f0bbd5fd19e833bef332c67d9a41a
VadimR <rakhmaninvs@gmail.com>
2022-09-14 18:45:38 +0300

    ex_005

---
+++ ex_005_HelloSpecial/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("insert your name");
+string? username = Console.ReadLine();
+if(username != null)
+{
+    if(username.ToLower() == "vadim")
+    {
+        Console.WriteLine("Hello my favorite user");
+    }
+    else
+    {
+        Console.WriteLine("Hello " + username);
+    }
+}
+else
+{
+    Console.WriteLine(" ");
+}
